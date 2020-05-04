a51fdca72b99eee9c3b700dd6dcc64bddd3fc62c
Dan <dan@instance.id>
2020-05-04 16:59:40 -0500

    entry

--- news-api/entrypoint.sh
+++ news-api/entrypoint.sh
@@ -7,4 +7,5 @@ host="-H 0.0.0.0"
 port="-p 9004"
 dest="http://instance.id/news/index.json"
 
-json-server $args $port $host $dest
+# json-server $args $port $host $dest
+json-server -p 9004 -H 0.0.0.0 http://instance.id/news/index.json

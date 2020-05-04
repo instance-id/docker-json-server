e5d1a37438e2641f1b0aa21aeccab3596a07db6e
Dan <dan@instance.id>
2020-05-04 16:54:35 -0500

    Config update

--- news-api/entrypoint.sh
+++ news-api/entrypoint.sh
@@ -3,8 +3,8 @@
 args="$@"
 
 args="$@ --static"
-
+host="-H 0.0.0.0"
 config="-c config.json"
 dest="http://instance.id/news/index.json"
 
-json-server $config $args $dest
+json-server $config $args $host $dest
